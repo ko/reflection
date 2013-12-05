5f92d0d57014f68a67a38a69355fc76ed4518b3a
ko <git@yaksok.net>
2013-12-04 21:16:04 -0800

    list storage dirs

--- android/src/com/relurori/reflection/MainActivity.java
+++ android/src/com/relurori/reflection/MainActivity.java
@@ -1,22 +1,67 @@
 package com.relurori.reflection;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+
 import android.os.Bundle;
+import android.os.Environment;
 import android.app.Activity;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.hardware.usb.UsbDevice;
+import android.hardware.usb.UsbManager;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
+	private final String TAG = MainActivity.class.getSimpleName();
+	
+	ListView deviceListView;
+	ArrayAdapter mArrayAdapter;
+	ArrayList<String> deviceArrayList;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		pre();
+		
+		populateStorageList();
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
+	private void pre() {
+		deviceArrayList = new ArrayList<String>();
+		deviceListView = (ListView) findViewById(R.id.deviceList);
+		mArrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,  
+                deviceArrayList);
+		
 	}
 
+	public void populateStorageList()
+	{
+	    File innerDir = Environment.getExternalStorageDirectory();	/* as /storage/emulated/0 */
+	    File rootDir = innerDir.getParentFile();	/* as /storage/emulated */
+	    rootDir = rootDir.getParentFile();			/* as /storage */
+	    File firstExtSdCard = innerDir ;
+	    File[] files = rootDir.listFiles();
+	    for (File file : files) {
+	    	if (file.listFiles() != null) {
+	    		deviceArrayList.add(file.toURI().toString() + "|" + file.listFiles().length);
+	    	}
+	    }
+
+	    deviceListView.setAdapter(mArrayAdapter);
+	}
 }
