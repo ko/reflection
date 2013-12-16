9efbe04b3de1a0a8f943988541eda9181a7b8235
Ken Ko <git@yaksok.net>
2013-12-16 00:36:44 -0800

    remove PagerAdapter required code for #7 and #8.

--- android/src/com/relurori/reflection/MainPagerAdapter.java
+++ android/src/com/relurori/reflection/MainPagerAdapter.java
@@ -34,11 +34,4 @@ public class MainPagerAdapter extends FragmentPagerAdapter {
 	public int getCount() {
 		return fragments.size();
 	}
-
-
-	@Override
-	public boolean isViewFromObject(View arg0, Object arg1) {
-		// TODO Auto-generated method stub
-		return false;
-	}
 }
