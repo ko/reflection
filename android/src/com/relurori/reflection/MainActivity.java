cdadeefe115af8f820a3eec83a0b76e631f95c77
Ken Ko <git@yaksok.net>
2013-12-15 23:18:24 -0800

    working on #7 and #8

--- android/src/com/relurori/reflection/MainActivity.java
+++ android/src/com/relurori/reflection/MainActivity.java
@@ -7,6 +7,8 @@ import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.List;
+import java.util.Vector;
 
 import android.net.Uri;
 import android.os.AsyncTask;
@@ -14,7 +16,6 @@ import android.os.Bundle;
 import android.os.Environment;
 import android.app.Activity;
 import android.app.Dialog;
-import android.app.Fragment;
 import android.app.PendingIntent;
 import android.app.ProgressDialog;
 import android.content.BroadcastReceiver;
@@ -23,6 +24,11 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.hardware.usb.UsbDevice;
 import android.hardware.usb.UsbManager;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
@@ -34,7 +40,7 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends Activity {
+public class MainActivity extends FragmentActivity {
 
 	private final String TAG = MainActivity.class.getSimpleName();
 	
@@ -42,45 +48,39 @@ public class MainActivity extends Activity {
 	ArrayAdapter mArrayAdapter;
 	Button button;
 	
+	private PagerAdapter mPagerAdapter = null;
+	
+	/*
 	private Fragment mTempFragment = null;
 	private InstructionFragment mInstructionFragment = new InstructionFragment();
 	private SelectSourceFragment mSourceFragment = new SelectSourceFragment();
 	private SelectDestinationFragment mDestinationFragment = new SelectDestinationFragment();
 	private SyncFragment mSyncFragment = new SyncFragment();
+	*/
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		setContentView(R.layout.activity_main_fragments);
 
 		pre();
 	}
 
+	private void fragmentsetup() {
+
+	}
+
 	private void pre() {
+		List<Fragment> fragments = new Vector<Fragment>();
+		fragments.add(Fragment.instantiate(this, InstructionFragment.class.getName()));
+		fragments.add(Fragment.instantiate(this, SelectSourceFragment.class.getName()));
+		fragments.add(Fragment.instantiate(this, SelectDestinationFragment.class.getName()));
+		fragments.add(Fragment.instantiate(this, SyncFragment.class.getName()));
 		
-		button = (Button)findViewById(R.id.clearDestinationDevice);
-		button.setOnClickListener(new View.OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				TextView tv = (TextView)findViewById(R.id.destinationDevice);
-				tv.setText("");
-			}
-		});
+		mPagerAdapter = new MainPagerAdapter(super.getSupportFragmentManager(), fragments);
 		
-		button = (Button)findViewById(R.id.clearSourceDevice);
-		button.setOnClickListener(new View.OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				TextView tv = (TextView)findViewById(R.id.sourceDevice);
-				tv.setText("");
-			}
-		});
-		
-		
-		
-		
+		ViewPager pager = (ViewPager)super.findViewById(R.id.viewpager);
+		pager.setAdapter(mPagerAdapter);
 	}
 
 }
---
+++ android/src/com/relurori/reflection/MainPagerAdapter.java
@@ -0,0 +1,31 @@
+package com.relurori.reflection;
+
+import java.util.List;
+
+/* Use support.v4.app because we use FragmentActivity. 
+ * Use FragmentActivity because we use FragmentPagerAdapter.
+ */
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+public class MainPagerAdapter extends FragmentPagerAdapter {
+
+	private List<Fragment> fragments;
+	
+	public MainPagerAdapter(FragmentManager fm, List<Fragment> frags) {
+		super(fm);
+		fragments = frags;
+	}
+
+
+	@Override
+	public Fragment getItem(int position) {
+		return fragments.get(position);
+	}
+	
+	@Override
+	public int getCount() {
+		return fragments.size();
+	}
+}
